0c65627760cb42281ad4a8a5b309f1dc40aa209b
Jaikumarjk <68479993+Jaikumarjk@users.noreply.github.com>
2020-07-19 01:25:30 +0530

    Add files via upload

---
+++ installscript.py
@@ -0,0 +1,7 @@
+import os
+os.system('pkg install bash')
+os.system('pkg install python')
+os.system('pkg install vim')
+os.system('pkg install w3m')
+os.system('pkg update')
+os.system('pkg upgrade')
